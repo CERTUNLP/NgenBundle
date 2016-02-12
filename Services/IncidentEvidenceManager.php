d3b013c171e87497a2cf98ad1d1bd0ce3e1472dd
dam <dam@himinbjorg>
2016-02-12 16:54:18 -0300

    mailer evidence management fix

--- Services/IncidentEvidenceManager.php
+++ Services/IncidentEvidenceManager.php
@@ -44,21 +44,22 @@ class IncidentEvidenceManager {
 
     public function setFilename(IncidentInterface $incident) {
         if ($incident->getEvidenceFile()) {
+            $ext = "_" . sha1(uniqid(mt_rand(), true));
             if (is_callable(array($incident->getEvidenceFile(), 'getClientOriginalExtension'))) {
 
-                $incident->setEvidenceFilePath($incident->getEvidenceFile()->getClientOriginalExtension());
+                $incident->setEvidenceFilePath($ext . "." . $incident->getEvidenceFile()->getClientOriginalExtension());
             } else {
 
-                $incident->setEvidenceFilePath($incident->getEvidenceFile()->getExtension());
+                $incident->setEvidenceFilePath($ext . "." . $incident->getEvidenceFile()->getExtension());
             }
         }
     }
 
     public function uploadEvidenceFile(IncidentInterface $incident) {
 
-        if (null === $incident->getEvidenceFile()) {
-            return;
-        }
+//        if (null === $incident->getEvidenceFile()) {
+//            return;
+//        }
         // if there is an error when moving the file, an exception will
         // be automatically thrown by move(). This will properly prevent
         // the entity from being persisted to the database on error
@@ -68,15 +69,16 @@ class IncidentEvidenceManager {
                 die('Failed to create folders...');
             }
         }
+
         $incident->getEvidenceFile()->move($uploadDir, $incident->getEvidenceFilePath());
 
         // check if we have an old image
-        if ($incident->getEvidenceFileTemp()) {
-            // delete the old image
-            unlink($this->getUploadDirectory() . $incident->getEvidenceFileTemp());
-            // clear the temp image path
-            $incident->setEvidenceFileTemp(null);
-        }
+//        if ($incident->getEvidenceFileTemp()) {
+//            // delete the old image
+//            unlink($this->getUploadDirectory() . $incident->getEvidenceFileTemp());
+//            // clear the temp image path
+//            $incident->setEvidenceFileTemp(null);
+//        }
     }
 
     protected function getUploadDirectory() {
