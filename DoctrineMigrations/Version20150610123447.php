fad705bc9ef91f0f21ad9250a42ba2b21b6cfc43
dam <dam@himinbjorg>
2016-02-24 10:49:55 -0300

    doctrine migrations changed path to internal bundle path

---
+++ DoctrineMigrations/Version20150610123447.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150610123447 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Incident CHANGE date date DATETIME NOT NULL, CHANGE last_time_detected last_time_detected DATETIME DEFAULT NULL, CHANGE renotification_date renotification_date DATETIME DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Incident CHANGE date date DATE NOT NULL, CHANGE last_time_detected last_time_detected DATE DEFAULT NULL, CHANGE renotification_date renotification_date DATE DEFAULT NULL');
+    }
+}
