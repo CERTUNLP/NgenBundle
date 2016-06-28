6363a0225d26e3b3a0fa6a014f2694f263dfdab1
drubio2 <drubio@cert.unlp.edu.ar>
2016-06-28 15:16:09 -0300

    fixes

--- Entity/Incident.php
+++ Entity/Incident.php
@@ -132,7 +132,7 @@ class Incident implements IncidentInterface {
     protected $state;
 
     /**
-     * @ORM\OneToOne(targetEntity="CertUnlp\NgenBundle\Entity\IncidentCommentThread",mappedBy="incident", fetch="EXTRA_LAZY")
+     * @ORM\OneToOne(targetEntity="CertUnlp\NgenBundle\Entity\IncidentCommentThread",inversedBy="incident")
      */
     private $comment_thread;
 
