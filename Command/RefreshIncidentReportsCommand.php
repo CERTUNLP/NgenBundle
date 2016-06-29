5908e19d3c00df0a06cebe67e3d6572705838695
drubio2 <drubio@cert.unlp.edu.ar>
2016-06-29 16:10:41 -0300

    mailer reports refactoring

--- Command/RefreshIncidentReportsCommand.php
+++ Command/RefreshIncidentReportsCommand.php
@@ -29,13 +29,12 @@ class RefreshIncidentReportsCommand extends ContainerAwareCommand {
     protected function execute(InputInterface $input, OutputInterface $output) {
         $output->writeln('[incidents:reports]: Starting.');
         $output->writeln('[incidents:reports]: Getting reports...');
-
         try {
             $output->writeln('[incidents:reports]: Parsing internal incident reports.');
             $this->parse_markdowns($this->getContainer()->getParameter('cert_unlp.ngen.incident.internal.report.markdown.path'), $this->getContainer()->getParameter('cert_unlp.ngen.incident.internal.report.twig.path'), $output);
             $output->writeln('[incidents:reports]: Done.');
             $output->writeln('[incidents:reports]: Parsing internal external reports.');
-            $this->parse_markdowns($this->getContainer()->getParameter('cert_unlp.ngen.incident.external.report.markdown.path'), $this->getContainer()->getParameter('cert_unlp.ngen.incident.external.report.twig.path'), $output);
+            $this->parse_markdowns($this->getContainer()->getParameter('cert_unlp.ngen.incident.external.report.markdown.path'), $this->getContainer()->getParameter('cert_unlp.ngen.incident.external.report.twig.path'), $output, false);
             $output->writeln('[incidents:reports]: Done.');
         } catch (Exception $ex) {
             $output->writeln('[incidents:reports]: Something is wrong.');
@@ -43,9 +42,20 @@ class RefreshIncidentReportsCommand extends ContainerAwareCommand {
         $output->writeln('[incidents:reports]: Done.');
     }
 
-    private function parse_markdowns($markdown_files_path, $twig_files_path, $output) {
+    private function parse_markdowns($markdown_files_path, $twig_files_path, $output, $internal = true) {
         $report_files = glob($markdown_files_path . '/*'); // get all file names
         $common_report_files = glob($markdown_files_path . '/common/*');
+        $header = "{# 
+ This file is part of the Ngen - CSIRT Incident Report System.
+ 
+ (c) CERT UNLP <support@cert.unlp.edu.ar>
+ 
+ This source file is subject to the GPL v3.0 license that is bundled
+ with this source code in the file LICENSE.
+#}
+{% set father = 'CertUnlpNgenBundle:" . ($internal ? "Internal" : "External") . "Incident:Report/Twig/BaseReport/baseReport.'~txtOrHtml~'.twig' %}
+{% extends father %}";
+
         foreach ($report_files as $file) { // iterate files
             $filename = basename($file);
             if (!in_array($filename, ['TODO.md', 'template.md', 'common'])) {
@@ -53,6 +63,7 @@ class RefreshIncidentReportsCommand extends ContainerAwareCommand {
                     $output->writeln('[incidents:reports]: parsing ' . $filename);
                 }
                 $html = $this->getContainer()->get('markdown.parser')->transformMarkdown(file_get_contents($file));
+                $html = $header . $html;
                 file_put_contents($twig_files_path . "/" . str_replace(".md", "Report.html.twig", basename($file)), $html);
             }
         }
@@ -62,7 +73,7 @@ class RefreshIncidentReportsCommand extends ContainerAwareCommand {
             if ($output->isVerbose()) {
                 $output->writeln('[incidents:reports]: parsing ' . $filename);
             }
-            $html = $this->getContainer()->get('markdown.parser')->transformMarkdown(file_get_contents($file), false);
+            $html = $this->getContainer()->get('markdown.parser')->transformMarkdown(file_get_contents($file));
             file_put_contents($twig_files_path . "/BaseReport/" . str_replace(".md", "Report.html.twig", basename($file)), $html);
         }
     }
