6d93d1753caf3ad0b22d12423da5ab699e01083f
Einar Lanfranco <einar@info.unlp.edu.ar>
2016-10-13 11:41:59 -0300

    instalador update

--- Resources/installer/install.es.sh
+++ Resources/installer/install.es.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+configurarApache(){
+    
+    read -p "Ingrese el nombre del host. Por defecto: "$(hostname) APACHE_HOSTNAME
+    if [ -z $APACHE_HOSTNAME ]
+       then
+       APACHE_HOSTNAME=$(hostname)
+    fi
+    
+    cp $WEB_PATH/dependencias/installer/ngen.conf /etc/apache2/sites-available/    
+    
+    sed -i "s/PATH_TO_WORKSPACE/$PATH_WEB/g" /etc/apache2/sites-available/ngen.conf ;   
+    sed -i "s/SERVER_NAME/$APACHE_HOSTNAME/g" /etc/apache2/sites-available/ngen.conf ;   
+    
+    $SUDO a2enmod rewrite ssl
+    $SUDO a2ensite ngen
+    $SUDO service apache2 restart
+    
+    }
+
 configurarParameters(){
     cp $WEB_PATH/dependencias/installer/parameters.yml $WEB_PATH/ngen_basic/app/config/parameters.yml
     read -p "Ingrese el host de la base de datos. Por defecto: 127.0.0.1)" DB_IP
@@ -67,9 +86,47 @@ configurarParameters(){
     
     }
 
+instalarElastic(){
+    $SUDO wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+    $SUDO echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+    $SUDO apt-get update;
+    $SUDO apt-get install apache2 php5 mysql-server php5-mysql php5-curl curl ant php-apc git elasticsearch
+    $SUDO update-rc.d elasticsearch defaults 95 10
+    }
+    
+instalarComposer(){
+    curl -sS https://getcomposer.org/installer | $SUDO php
+    }
+    
+instalarSymfony(){
+    $SUDO curl -LsS http://symfony.com/installer -o ../binarios/symfony
+    $SUDO chmod +x ../binarios/symfony
+}
 
-
-
+instalarNode(){
+    curl -sL https://deb.nodesource.com/setup_4.x | $SUDO bash -
+    $SUDO apt-get install --yes nodejs
+    $SUDO ln -s /usr/bin/nodejs /usr/bin/node
+    $SUDO npm install -g less
+    }
+    
+configurarDependencias(){
+    cd $PATH_WEB/dependencias
+    cd ../binarios
+    instalarElastic
+    instalarComposer
+    cd $OLDPWD
+    instalarSymfony
+    instalarNode
+    }
+    
+instalarNgen(){
+    ./binarios/symfony new ngen_basic 2.8
+    cd ngen_basic
+    php ../binarios/composer.phar require certunlp/ngen-bundle:dev-master
+    php ../binarios/composer.phar update
+}
+    
 clear
 echo "# -----------------------------------------------------------"
 echo "# ------------------------- Instalando NGEN------------------"
@@ -115,67 +172,15 @@ fi
 if [[ $METELE != 0 ]]; then
     mkdir -p $PATH_WEB/dependencias;
     mkdir -p $PATH_WEB/binarios
-    cd $PATH_WEB/dependencias
-    $SUDO wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-    $SUDO echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
-    $SUDO apt-get update;
-    $SUDO apt-get install apache2 php5 mysql-server php5-mysql php5-curl curl ant php-apc git elasticsearch
-    cd ../binarios
-    curl -sS https://getcomposer.org/installer | $SUDO php
-    cd $OLDPWD
-    $SUDO curl -LsS http://symfony.com/installer -o ../binarios/symfony
-    $SUDO chmod +x ../binarios/symfony
-    $SUDO a2enmod rewrite ssl
-    $SUDO update-rc.d elasticsearch defaults 95 10
-    curl -sL https://deb.nodesource.com/setup_4.x | $SUDO bash -
-    $SUDO apt-get install --yes nodejs
-    $SUDO ln -s /usr/bin/nodejs /usr/bin/node
-    $SUDO npm install -g less
+    configurarDependencias
     cd $PATH_WEB
-    ./binarios/symfony new ngen_basic 2.8
-    cd ngen_basic
-    php ../binarios/composer.phar require certunlp/ngen-bundle:dev-master
-    php ../binarios/composer.phar update
+    instalarNgen
     configurarApache
     configurarParameters
     configurarAppKernel
 fi;
 
 
-
-#~ ### Configure apache virtualhost
-#~ #### Basic config
-    #~ <VirtualHost *:80>
-        #~ ServerAdmin     cert@cert.com
-        #~ ServerName      ngen.com
-        #~ ServerAlias     ngen.com 
-        #~ DocumentRoot "$(PATH_TO_WORKSPACE)/web"
-        #~ RewriteEngine On
-        #~ <Directory "$(PATH_TO_WORKSPACE)/web">
-            #~ # enable the .htaccess rewrites
-            #~ Options Indexes FollowSymLinks MultiViews
-            #~ AllowOverride All
-            #~ Order allow,deny
-            #~ Allow from All
-        #~ </Directory>
-    #~ </VirtualHost>
-#~ 
-#~ ### Edit app/config/parameters.yml
-#~ #### add your DB config
-#~ #### create the database_user manually 
-    #~ parameters:
-        #~ database_host: 127.0.0.1
-        #~ database_port: null
-        #~ database_name: symfony
-        #~ database_user: root
-        #~ database_password: null
-        #~ mailer_transport: smtp
-        #~ mailer_host: 127.0.0.1
-        #~ mailer_user: null
-        #~ mailer_password: null
-        #~ secret: b9622e02c564144683568d638137aed04f61359c
-#~ 
-#~ 
 #~ ### Add the routing resource to your app/AppKernel.php
     #~ new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
     #~ new FOS\RestBundle\FOSRestBundle(),
