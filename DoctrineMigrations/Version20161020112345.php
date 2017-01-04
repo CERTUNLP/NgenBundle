64be0548d8e45bbc95fa5636b5614ec66b10c0bb
drubio <drubio@cert.unlp.edu.ar>
2017-01-04 11:29:17 -0300

    Merge develop
    Conflicts:
    	Resources/config/config.yml

--- DoctrineMigrations/Version20161020112345.php
+++ DoctrineMigrations/Version20161020112345.php
@@ -4,17 +4,38 @@ namespace Application\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Gedmo\Sluggable\Util as Sluggable;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20161020112345 extends AbstractMigration
-{
+class Version20161020112345 extends AbstractMigration implements ContainerAwareInterface {
+
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+
+    public function postUp(Schema $schema) {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+
+        $qb = $em->createQueryBuilder();
+        $network_admins = $qb->select('na')
+                        ->from('CertUnlpNgenBundle:NetworkAdmin', 'na')->getQuery()->getResult();
+        foreach ($network_admins as $network_admin) {
+            $network_admin->setSlug(Sluggable\Urlizer::urlize($network_admin->getName() . " " . $network_admin->getEmail(), '_'));
+            $em->persist($network_admin);
+        }
+        $em->flush();
+    }
+
     /**
      * @param Schema $schema
      */
-    public function up(Schema $schema)
-    {
+    public function up(Schema $schema) {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
@@ -24,11 +45,11 @@ class Version20161020112345 extends AbstractMigration
     /**
      * @param Schema $schema
      */
-    public function down(Schema $schema)
-    {
+    public function down(Schema $schema) {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE incident_state DROP is_active, DROP created_at, DROP updated_at');
     }
+
 }
