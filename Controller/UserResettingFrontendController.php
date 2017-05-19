81f8882ee91fcc485d04f238e48e5fd4cdd80eea
drubio <drubio@cert.unlp.edu.ar>
2017-05-19 15:10:16 -0300

    user reset password profile and login

---
+++ Controller/UserResettingFrontendController.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * This file is part of the Ngen - CSIRT Incident Report System.
+ *
+ * (c) CERT UNLP <support@cert.unlp.edu.ar>
+ *
+ * This source file is subject to the GPL v3.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace CertUnlp\NgenBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+use CertUnlp\NgenBundle\Form\Model\UserChangePassword;
+use CertUnlp\NgenBundle\Form\UserChangePasswordType;
+use FOS\UserBundle\Controller\ResettingController;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+
+class UserResettingFrontendController extends ResettingController {
+
+    /**
+     * @Route("/request", name="fos_user_resetting_request", methods="GET")
+     */
+    public function requestAction() {
+        return $this->render('CertUnlpNgenBundle:User:Frontend/Resetting/request.html.twig');
+    }
+
+    /**
+     * @Route("/send-email", name="fos_user_resetting_send_email", methods="POST")
+     */
+    public function sendEmailAction(Request $request) {
+        return parent::sendEmailAction($request);
+    }
+
+    /**
+     * @Route("/check-email", name="fos_user_resetting_check_email", methods="GET")
+     */
+    public function checkEmailAction(Request $request) {
+        $response = parent::checkEmailAction($request);
+        $response->setContent($this->renderView('CertUnlpNgenBundle:User:Frontend/Resetting/check_email.html.twig'));
+        return $response;
+    }
+
+    /**
+     * @Route("/reset/{token}", name="fos_user_resetting_reset")
+     * @Method({"GET", "POST"})
+     */
+    public function resetAction(Request $request, $token) {
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->get('fos_user.resetting.form.factory');
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->get('fos_user.user_manager');
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->get('event_dispatcher');
+
+        $user = $userManager->findUserByConfirmationToken($token);
+
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with "confirmation token" does not exist for value "%s"', $token));
+        }
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::RESETTING_RESET_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(FOSUserEvents::RESETTING_RESET_SUCCESS, $event);
+
+            $userManager->updateUser($user);
+
+            if (null === $response = $event->getResponse()) {
+                $url = $this->generateUrl('fos_user_profile_show');
+                $response = new RedirectResponse($url);
+            }
+
+            $dispatcher->dispatch(
+                    FOSUserEvents::RESETTING_RESET_COMPLETED, new FilterUserResponseEvent($user, $request, $response)
+            );
+
+            return $response;
+        }
+
+        return $this->render('CertUnlpNgenBundle:User:Frontend/Resetting/reset.html.twig', array(
+                    'token' => $token,
+                    'form' => $form->createView(),
+        ));
+    }
+
+}
