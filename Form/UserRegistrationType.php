199c3003a6835cfb151bdac7c695098f1f7d6c9a
drubio <drubio@cert.unlp.edu.ar>
2017-05-22 14:24:31 -0300

    user registration

---
+++ Form/UserRegistrationType.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Ngen - CSIRT Incident Report System.
+ *
+ * (c) CERT UNLP <support@cert.unlp.edu.ar>
+ *
+ * This source file is subject to the GPL v3.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace CertUnlp\NgenBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserRegistrationType extends AbstractType {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('name')
+                ->add('lastname');
+    }
+
+    public function getParent() {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+
+    public function getBlockPrefix() {
+        return 'app_user_registration';
+    }
+
+    // For Symfony 2.x
+    public function getName() {
+        return $this->getBlockPrefix();
+    }
+
+}
