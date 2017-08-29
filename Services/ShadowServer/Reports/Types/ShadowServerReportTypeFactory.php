c8870e60948d3a698bdbf8bba52db7e6f3f8f2cf
drubio <drubio@cert.unlp.edu.ar>
2017-08-29 16:44:41 -0300

    new shadowservers report

--- Services/ShadowServer/Reports/Types/ShadowServerReportTypeFactory.php
+++ Services/ShadowServer/Reports/Types/ShadowServerReportTypeFactory.php
@@ -12,6 +12,7 @@
 namespace CertUnlp\NgenBundle\Services\ShadowServer\Reports\Types;
 
 use CertUnlp\NgenBundle\Services\ShadowServer\Reports\ShadowServerCsvRow;
+use CertUnlp\NgenBundle\Services\ShadowServer\Reports\Types\ShadowServerReport;
 
 /**
  * Description of TypeFactory
@@ -22,14 +23,38 @@ class ShadowServerReportTypeFactory {
 
     public function getReportTypeFromCsvRow($csv_row, $file_path, $csv_evidence_file) {
         $file = explode('-', basename($file_path))[0];
-        $shadow_server_csv_row = new ShadowServerCsvRow($csv_row, $file);
-        $type = __NAMESPACE__ . "\\" . preg_replace('/(?:^|_)(.?)/e', "strtoupper('$1')", $file); //CamelCase
-        if (class_exists($type)) {
-            return new $type($shadow_server_csv_row, $csv_evidence_file);
-        } else {
-            echo "[shadowserver]The class $type does not exist \n";
-            return null;
+        $file = str_replace("scan", "open", $file);
+        switch ($file) {
+            case 'dns_openresolver':
+                $file = 'open_dns';
+                break;
+            case 'open_ntp':
+                $file = 'open_ntp_version';
+                break;
+            case 'open_ntpmonitor':
+                $file = 'open_ntp_monitor';
+                break;
+            case 'open_portmapper':
+                $file = 'open_portmap';
+                break;
+            case 'open_ssl_poodle':
+                $file = 'poodle';
+                break;
+
+            default:
+                break;
         }
+        $shadow_server_csv_row = new ShadowServerCsvRow($csv_row, $file);
+//        var_dump(str_replace("scan", "open", $file));
+//        $type = __NAMESPACE__ . "\\" . preg_replace_callback('/(?:^|_)(.?)/', function ($coincidencias) {
+//                    return strtoupper($coincidencias[0]);
+//                }, $file); //CamelCase
+//        if (class_exists($type)) {
+        return new ShadowServerReport($shadow_server_csv_row, $csv_evidence_file);
+//        } else {
+//            echo "[shadowserver]The class $type does not exist \n";
+//            return null;
+//        }
     }
 
 }
