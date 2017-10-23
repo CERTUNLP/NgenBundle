926349d2116969c11332aef82005172b15aebb03
drubio <drubio@cert.unlp.edu.ar>
2017-10-23 13:23:32 -0300

    fix csrf

--- Form/IncidentFeedType.php
+++ Form/IncidentFeedType.php
@@ -52,7 +52,8 @@ class IncidentFeedType extends AbstractType {
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
-            'data_class' => 'CertUnlp\NgenBundle\Entity\IncidentFeed'
+            'data_class' => 'CertUnlp\NgenBundle\Entity\IncidentFeed',
+            'csrf_protection' => false,
         ));
     }
 
