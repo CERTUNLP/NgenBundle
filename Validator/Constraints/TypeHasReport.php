cadcf98ec01922d39b0c7fa7487841e0aafff2e4
drubio <drubio@cert.unlp.edu.ar>
2017-12-12 14:59:18 -0300

    incident types migration to DB

---
+++ Validator/Constraints/TypeHasReport.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Ngen - CSIRT Incident Report System.
+ *
+ * (c) CERT UNLP <support@cert.unlp.edu.ar>
+ *
+ * This source file is subject to the GPL v3.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace CertUnlp\NgenBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class TypeHasReport extends Constraint {
+
+    public $message = 'This type does\'n have a report for this language ("%string%")';
+
+    public function validatedBy() {
+        return 'type.has.report.validator';
+    }
+
+}
