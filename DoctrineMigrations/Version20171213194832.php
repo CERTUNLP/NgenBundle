042fdee1bd224ba4a697a39b7ba39903044a810a
drubio <drubio@cert.unlp.edu.ar>
2017-12-13 16:52:14 -0300

    command, elastica fix

---
+++ DoctrineMigrations/Version20171213194832.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171213194832 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE incident_report DROP PRIMARY KEY');
+        $this->addSql('ALTER TABLE incident_report CHANGE type type VARCHAR(100) DEFAULT NULL');
+        $this->addSql('ALTER TABLE incident_report ADD PRIMARY KEY (slug)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE incident_report DROP PRIMARY KEY');
+        $this->addSql('ALTER TABLE incident_report CHANGE type type VARCHAR(100) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE incident_report ADD PRIMARY KEY (lang, type)');
+    }
+}
