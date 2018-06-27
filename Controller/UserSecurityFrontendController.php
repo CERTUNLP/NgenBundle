79a0dece246ab5db798302a7fcc556e925597e96
dam <rubiodamian22@gmail.com>
2018-06-27 12:10:54 -0300

    FOS user fix

--- Controller/UserSecurityFrontendController.php
+++ Controller/UserSecurityFrontendController.php
@@ -11,40 +11,44 @@
 
 namespace CertUnlp\NgenBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\SecurityContext;
 use CertUnlp\NgenBundle\Form\Model\UserChangePassword;
-use CertUnlp\NgenBundle\Form\UserChangePasswordType;
 use FOS\UserBundle\Controller\SecurityController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class UserSecurityFrontendController extends SecurityController {
+class UserSecurityFrontendController extends SecurityController
+{
+    public function __construct()
+    {
+    }
 
     /**
      * @Route("/login", name="fos_user_security_login")
      */
-    public function loginAction(Request $request) {
+    public function loginAction(Request $request)
+    {
         return parent::loginAction($request);
-    }
-
-    protected function renderLogin(array $data) {
-        return $this->render('CertUnlpNgenBundle:User:Frontend/login.html.twig', $data);
     }
 
     /**
      * @Route("/login_check", name="fos_user_security_check")
      */
-    public function checkAction() {
+    public function checkAction()
+    {
         return $this->redirect("/");
     }
 
     /**
      * @Route("/logout", name="fos_user_security_logout")
      */
-    public function logoutAction() {
+    public function logoutAction()
+    {
         return parent::logoutAction();
+    }
+
+    protected function renderLogin(array $data)
+    {
+        return $this->render('CertUnlpNgenBundle:User:Frontend/login.html.twig', $data);
     }
 
 }
