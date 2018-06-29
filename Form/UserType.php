67c8faa1ba658229cb2161dbdedefabd6e82ebaa
drubio <drubio@cert.unlp.edu.ar>
2018-06-29 13:30:24 -0300

    user register fix

--- Form/UserType.php
+++ Form/UserType.php
@@ -11,15 +11,46 @@
 
 namespace CertUnlp\NgenBundle\Form;
 
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use FOS\UserBundle\Util\LegacyFormHelper;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
 
-class UserType extends AbstractType {
+class UserType extends AbstractType
+{
 
-    private function roleChoices() {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $choices = $this->roleChoices();
+        $builder->add('name')
+            ->add('lastname')
+            ->add('plainPassword', RepeatedType::class, array(
+                'required' => false,
+                'type' => PasswordType::class,
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+            ->add('roles', ChoiceType::class, array(
+                'label' => 'Roles',
+                'choices' => $choices,
+                'expanded' => true,
+                'multiple' => true,
+                'data' => $builder->getData() ? $builder->getData()->getRoles() : []
+            ))
+            ->add('save', 'submit', array(
+                'attr' => array('class' => 'save ladda-button btn-lg btn-block', 'data-style' => 'slide-down')));
+    }
+
+    private function roleChoices()
+    {
         $choices = array(
             'ROLE_API' => 'ROLE_API',
             'ROLE_ADMIN' => 'ROLE_ADMIN',
@@ -28,47 +59,22 @@ class UserType extends AbstractType {
         return $choices;
     }
 
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options) {
-        $choices = $this->roleChoices();
-        $builder->add('name')
-                ->add('lastname')
-                ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
-                    'required' => false,
-                    'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
-                    'options' => array('translation_domain' => 'FOSUserBundle'),
-                    'first_options' => array('label' => 'form.password'),
-                    'second_options' => array('label' => 'form.password_confirmation'),
-                    'invalid_message' => 'fos_user.password.mismatch',
-                ))
-                ->add('roles', ChoiceType::class, array(
-                    'label' => 'Roles',
-                    'choices' => $choices,
-                    'expanded' => true,
-                    'multiple' => true,
-                    'data' => $builder->getData() ? $builder->getData()->getRoles() : []
-                ))
-                ->add('save', 'submit', array(
-                    'attr' => array('class' => 'save ladda-button btn-lg btn-block', 'data-style' => 'slide-down')))
-        ;
-    }
-
-    public function getParent() {
+    public function getParent()
+    {
         return 'FOS\UserBundle\Form\Type\RegistrationFormType';
 
         // Or for Symfony < 2.8
         // return 'fos_user_registration';
     }
 
-    public function getBlockPrefix() {
+    public function getBlockPrefix()
+    {
         return '';
     }
 
     // For Symfony 2.x
-    public function getName() {
+    public function getName()
+    {
         return '';
     }
 
