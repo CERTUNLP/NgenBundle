daa763fee8563f6e9da7b11dfc3d2d2a2370d9cb
Jeremías Pretto <jpretto@cert.unlp.edu.ar>
2018-11-14 18:41:06 -0300

    Layout view

--- Controller/AcademicUnitFrontendController.php
+++ Controller/AcademicUnitFrontendController.php
@@ -2,9 +2,9 @@
 
 /*
  * This file is part of the Ngen - CSIRT Incident Report System.
- * 
+ *
  * (c) CERT UNLP <support@cert.unlp.edu.ar>
- * 
+ *
  * This source file is subject to the GPL v3.0 license that is bundled
  * with this source code in the file LICENSE.
  */
@@ -28,7 +28,7 @@ class AcademicUnitFrontendController extends Controller
 
     /**
      * @Template("CertUnlpNgenBundle:AcademicUnit:Frontend/home.html.twig")
-     * @Route("/", name="cert_unlp_ngen_academic_unit_frontend_home")
+     * @Route("/", name="cert_unlp_ngen_network_network_entity_frontend_home")
      * @param Request $request
      * @return array
      */
