f261a8130344aff9f574004fa8c061918c36e3bf
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2018-12-06 17:38:58 -0300

    pasando todo

---
+++ Controller/Frontend/Incident/IncidentPriorityFrontendController.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * This file is part of the Ngen - CSIRT Incident Report System.
+ *
+ * (c) CERT UNLP <support@cert.unlp.edu.ar>
+ *
+ * This source file is subject to the GPL v3.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * Description of IncidentPriorityFrontendController
+ *
+ * @author einar
+ */
+
+namespace CertUnlp\NgenBundle\Controller\Frontend\Incident;
+
+use CertUnlp\NgenBundle\Entity\Incident\IncidentPriority;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class IncidentPriorityFrontendController extends Controller
+{
+
+    /**
+     * @Template("CertUnlpNgenBundle:IncidentPriority:Frontend/home.html.twig")
+     * @Route("/", name="cert_unlp_ngen_incident_priority_frontend_home")
+     * @param Request $request
+     * @return array
+     */
+    public function homeAction(Request $request)
+    {
+        return $this->getFrontendController()->homeEntity($request);
+    }
+
+    public function getFrontendController()
+    {
+        return $this->get('cert_unlp.ngen.incident.priority.frontend.controller');
+    }
+
+    /**
+     * @Template("CertUnlpNgenBundle:IncidentPriority:Frontend/home.html.twig")
+     * @Route("search", name="cert_unlp_ngen_incident_priority_search")
+     * @param Request $request
+     * @return array
+     */
+    public function searchIncidentPriorityAction(Request $request)
+    {
+        return $this->getFrontendController()->searchEntity($request);
+    }
+
+    /**
+     * @Template("CertUnlpNgenBundle:IncidentPriority:Frontend/incidentPriorityForm.html.twig")
+     * @Route("/new", name="cert_unlp_ngen_incident_priority_new")
+     * @param Request $request
+     * @return array
+     */
+    public function newIncidentPriorityAction(Request $request)
+    {
+        return $this->getFrontendController()->newEntity($request);
+    }
+
+    /**
+     * @Template("CertUnlpNgenBundle:IncidentPriority:Frontend/incidentPriorityForm.html.twig")
+     * @Route("{id}/edit", name="cert_unlp_ngen_incident_priority_edit")
+     * @param IncidentPriority $IncidentPriority
+     * @return array
+     */
+    public function editIncidentPriorityAction(IncidentPriority $IncidentPriority)
+    {
+        return $this->getFrontendController()->editEntity($IncidentPriority);
+    }
+
+    /**
+     * @Template("CertUnlpNgenBundle:IncidentPriority:Frontend/incidentPriorityDetail.html.twig")
+     * @Route("{id}/detail", name="cert_unlp_ngen_incident_priority_detail")
+     * @param IncidentPriority $IncidentPriority
+     * @return array
+     */
+    public function detailIncidentPriorityAction(IncidentPriority $IncidentPriority)
+    {
+        return $this->getFrontendController()->detailEntity($IncidentPriority);
+    }
+
+}
---
+++ Resources/public/js/incident/priority/IncidentPriorityForm.js
@@ -0,0 +1,40 @@
+/* 
+ * This file is part of the Ngen - CSIRT Incident Report System.
+ * 
+ * (c) CERT UNLP <support@cert.unlp.edu.ar>
+ * 
+ * This source file is subject to the GPL v3.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+var IncidentPriorityForm = Form.extend({
+    config: function () {
+        this.setIncidentPriorityId();
+    },
+    getObjectBrief: function () {
+        return 'incident/priority';
+    },
+    getObjectId: function () {
+        return this.getIncidentPriorityId();
+    },
+    setIncidentPriorityId: function () {
+        this.incident_priority_id = (($('#feed').val().replace(' ', '_'))).toLowerCase();
+    },
+    getIncidentPriorityId: function () {
+        return this.incident_priority_id;
+    },
+    handleExtraErrors: function (jqXHR) {
+//        $.each(jqXHR.responseJSON.errors.errors, function (k, v) {
+//            errorsText = "";
+//            if (v.length > 0) {
+//                ul = $('<ul class="help-block" ></ul>');
+//                ul.append($('<li>' + v + '</li>'));
+//                $('#ip').after(ul);
+//                $('#ip').closest('div[class="form-group"]').addClass('has-error');
+//            } else {
+//                $('#ip').closest('div[class="form-group has-error"]').removeClass('has-error');
+//                $('#ip').siblings('ul').remove();
+//            }
+//        });
+    }
+});
+
