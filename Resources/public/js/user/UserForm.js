74128b5a32d0ef36e4c03ad5359439e3d8c4ec33
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2019-04-01 19:49:09 -0300

    varios avances de vista

--- Resources/public/js/user/UserForm.js
+++ Resources/public/js/user/UserForm.js
@@ -22,29 +22,92 @@ var UserForm = Form.extend({
     getUserId: function () {
         return this.userId;
     },
-    handleExtraErrors: function (jqXHR) {
-        function ya(jqXHR.responseJSON.errors.children) {
-            console.log(obj.name);
-            if (obj.child) {
-                console.log("length=" + obj.child.length);
-                for (i = 0; i < obj.child.length; i++) {
-                    ya(obj.child[i]);
+    handleExtraErrors: function (xhr) {
+        var data = $.parseJSON(xhr.responseText);
+
+        if (data.messages) {
+            var messages = data.messages;
+
+            var i;
+
+            if (messages.error) {
+                for (i = 0; i < messages.error.length; i++) {
+                    methods.addError(messages.error[i]);
+                }
+            }
+
+            if (messages.success) {
+                for (i = 0; i < messages.success.length; i++) {
+                    methods.addSuccess(messages.success[i]);
+                }
+            }
+
+            if (messages.info) {
+                for (i = 0; i < messages.info.length; i++) {
+                    methods.addInfo(messages.info[i]);
                 }
             }
         }
-        $.each(jqXHR.responseJSON.errors.children, function (k, v) {
-            errorsText = "";
-            if (! jQuery.isEmptyObject(v)    ) {
-                ul = $('<ul class="help-block" ></ul>');
-                ul.append($('<li>' + JSON.stringify(v,) + '</li>'));
-                $('#error').after(ul);
-                $('#error').closest('div[class="form-group"]').addClass('has-error');
-            } else {
-                $('#error').closest('div[class="form-group has-error"]').removeClass('has-error');
-                $('#error').siblings('ul').remove();
-            }
-        });
+    },
+    addSuccess: function(message) {
+        var flashMessageElt = methods.getBasicFlash(message).addClass('alert-success');
 
-    }
+        methods.addToList(flashMessageElt);
+        methods.display(flashMessageElt);
+    },
+
+    addError: function(message) {
+        var flashMessageElt = methods.getBasicFlash(message).addClass('alert-error');
+
+        methods.addToList(flashMessageElt);
+        methods.display(flashMessageElt);
+    },
+
+    addInfo: function(message) {
+        var flashMessageElt = methods.getBasicFlash(message).addClass('alert-info');
+
+        methods.addToList(flashMessageElt);
+        methods.display(flashMessageElt);
+    },
+
+    getBasicFlash: function(message) {
+        var flashMessageElt = $('<div></div>')
+            .hide()
+            .addClass('alert')
+            .append(methods.getCloseButton())
+            .append($('<div></div>').html(message))
+        ;
+
+        return flashMessageElt;
+    },
+
+    getCloseButton: function() {
+        var closeButtonElt = $('<button></button>')
+            .addClass('close')
+            .attr('data-dismiss', 'alert')
+            .html('&times')
+        ;
+
+        return closeButtonElt;
+    },
+
+    addToList: function(flashMessageElt) {
+        flashMessageElt.appendTo($('#flash-messages'));
+    },
+
+    display: function(flashMessageElt) {
+        setTimeout(
+            function() {
+                flashMessageElt
+                    .show('slow')
+                    .delay(methods.settings.hideDelay)
+                    .hide('fast', function() { $(this).remove(); } )
+                ;
+            },
+            500
+        );
+    },
+    );
 });
+
 
