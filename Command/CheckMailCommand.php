86f533f83d4954334480acc94de4ddd50cef3f7e
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2019-04-30 17:29:15 -0300

    Check mail command developed formated code

--- Command/CheckMailCommand.php
+++ Command/CheckMailCommand.php
@@ -33,49 +33,49 @@ class CheckMailCommand extends ContainerAwareCommand
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
-    {   $connection=$input->getOption('connection');
-        $mark=$input->getOption('mark');
-        $mailbot=$input->getOption('mailbot');
+    {
+        $connection = $input->getOption('connection');
+        $mark = $input->getOption('mark');
+        $mailbot = $input->getOption('mailbot');
         $output->writeln('<info>[Messages]: Starting.</info>');
         $output->writeln('<info>[Messages]: Searching for new mail...</info>');
         $ngen_Connection = $this->getContainer()->get('secit.imap')->get($connection);
-        $comment_thread_manager= $this->getContainer()->get('fos_comment.manager.thread');
-        $comment_manager= $this->getContainer()->get('fos_comment.manager.comment');
+        $comment_thread_manager = $this->getContainer()->get('fos_comment.manager.thread');
+        $comment_manager = $this->getContainer()->get('fos_comment.manager.comment');
         $info = $ngen_Connection->getMailboxInfo();
-        $output->writeln('<info>[Mails]: We found '. $info->Unread.' new messages...</info>');
+        $output->writeln('<info>[Mails]: We found ' . $info->Unread . ' new messages...</info>');
         $emails = $ngen_Connection->searchMailBox('UNSEEN');
         //If the $emails variable is not a boolean FALSE value or
         //an empty array.
-        $userMailbot=$this->findMailbotUser($mailbot);
-        if (!$userMailbot){
-            $output->writeln('<error>[Mails]: No username '.$mailbot. ' detected, you have to do it! </error>');
+        $userMailbot = $this->findMailbotUser($mailbot);
+        if (!$userMailbot) {
+            $output->writeln('<error>[Mails]: No username ' . $mailbot . ' detected, you have to do it! </error>');
             die();
         }
-        if(!empty($emails)){
+        if (!empty($emails)) {
             //Loop through the emails.
             echo "entrando";
-            foreach($emails as $email){
-                $output->writeln("<info>Procesando email con id ".$email.'<info>');
-                $subject= $ngen_Connection->getMailHeader($email)->subject;
-                if (preg_match("/^Re\:/",$subject) && preg_match('/\[CERTUNLP\]/',$subject)){
-                    preg_match('/\[ID:(?P<id>\d+)\]/',$subject,$incident_id);
-                    $raw=$ngen_Connection->getMail($email,$mark);
-                    $mensaje= preg_replace('#(^\w.+:\n)?(^>.*(\n|$))+#mi', '', $raw->textPlain);
-                    $mensaje2=explode("--",$mensaje);
+            foreach ($emails as $email) {
+                $output->writeln("<info>Procesando email con id " . $email . '<info>');
+                $subject = $ngen_Connection->getMailHeader($email)->subject;
+                if (preg_match("/^Re\:/", $subject) && preg_match('/\[CERTUNLP\]/', $subject)) {
+                    preg_match('/\[ID:(?P<id>\d+)\]/', $subject, $incident_id);
+                    $raw = $ngen_Connection->getMail($email, $mark);
+                    $mensaje = preg_replace('#(^\w.+:\n)?(^>.*(\n|$))+#mi', '', $raw->textPlain);
+                    $mensaje2 = explode("--", $mensaje);
                     $mensaje = substr($mensaje2[0], 0, strrpos($mensaje2[0], "\n"));
                     $mensaje3 = substr($mensaje, 0, strrpos($mensaje, "\n"));
-                    $body=$raw->fromName." <".$raw->fromAddress.">  answered: ".$mensaje3;
-                    $incident=$this->findIncidentToUpdate($incident_id['id']);
-                    if ($incident){
+                    $body = $raw->fromName . " <" . $raw->fromAddress . ">  answered: " . $mensaje3;
+                    $incident = $this->findIncidentToUpdate($incident_id['id']);
+                    if ($incident) {
                         $thread = $comment_thread_manager->findThreadById($incident_id['id']);
-                        $comment= $comment_manager->createComment($thread);
+                        $comment = $comment_manager->createComment($thread);
                         $comment->setBody($body);
                         $comment->setAuthor($userMailbot);
                         $comment_manager->saveComment($comment);
-                        $output->writeln('<info>[Messages]: Persisting comment for incident '.$incident->getSlug().' messages.</info>');
-                    }
-                    else {
-                        $output->writeln('<error>[Messages]: The incident '.$incident_id['id'].' does not exist.</error>');
+                        $output->writeln('<info>[Messages]: Persisting comment for incident ' . $incident->getSlug() . ' messages.</info>');
+                    } else {
+                        $output->writeln('<error>[Messages]: The incident ' . $incident_id['id'] . ' does not exist.</error>');
 
                     }
                 }
@@ -91,7 +91,7 @@ class CheckMailCommand extends ContainerAwareCommand
      */
     private function findIncidentToUpdate($id): ?Incident
     {
-        return $this->getContainer()->get('doctrine')->getRepository(Incident::class)->findOneBy(['id'=>$id]);
+        return $this->getContainer()->get('doctrine')->getRepository(Incident::class)->findOneBy(['id' => $id]);
     }
 
     /**
@@ -99,7 +99,7 @@ class CheckMailCommand extends ContainerAwareCommand
      */
     private function findMailbotUser($username): ?User
     {
-        return $this->getContainer()->get('doctrine')->getRepository(User::class)->findOneBy(['username'=>$username]);
+        return $this->getContainer()->get('doctrine')->getRepository(User::class)->findOneBy(['username' => $username]);
     }
 
 }
