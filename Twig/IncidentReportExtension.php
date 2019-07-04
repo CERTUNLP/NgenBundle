c39ece675b65f334ceedc12d45a7bad30a8c2d7a
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2019-07-04 17:34:49 -0300

    sacando el bundle de barinstoms

--- Twig/IncidentReportExtension.php
+++ Twig/IncidentReportExtension.php
@@ -13,6 +13,7 @@ namespace CertUnlp\NgenBundle\Twig;
 
 use Twig_Extension;
 use Twig_SimpleFunction;
+use Twig_SimpleFilter;
 
 class IncidentReportExtension extends Twig_Extension
 {
@@ -27,7 +28,12 @@ class IncidentReportExtension extends Twig_Extension
             new \Twig_SimpleFilter('emphasized', array($this, 'emphasizedFilter'), array('is_safe' => array('html'))),
             new \Twig_SimpleFilter('urlLink', array($this, 'urlLinkFilter'), array('is_safe' => array('html'))),
             new \Twig_SimpleFilter('list', array($this, 'listFilter'), array('is_safe' => array('html'))),
-        );
+            new Twig_SimpleFilter(
+                'parse_icons',
+                array($this, 'parseIconsFilter'),
+                array('pre_escape' => 'html', 'is_safe' => array('html'))
+            )
+            );
     }
 
     public function paragraphFilter($text)
@@ -103,7 +109,12 @@ class IncidentReportExtension extends Twig_Extension
         new Twig_SimpleFunction('label_success', array($this, 'labelSuccessFunction'), $options),
         new Twig_SimpleFunction('label_info', array($this, 'labelInfoFunction'), $options),
         new Twig_SimpleFunction('label_warning', array($this, 'labelWarningFunction'), $options),
-        new Twig_SimpleFunction('label_danger', array($this, 'labelDangerFunction'), $options)
+        new Twig_SimpleFunction('label_danger', array($this, 'labelDangerFunction'), $options),
+        new Twig_SimpleFunction(
+                    'icon',
+                    array($this, 'iconFunction'),
+                    array('pre_escape' => 'html', 'is_safe' => array('html'))
+                )
     );
 }
 
@@ -177,5 +188,51 @@ public function labelInfoFunction($text)
 {
     return $this->labelFunction($text, 'info');
 }
+
+    /**
+     * @var string
+     */
+    private $iconPrefix='fa';
+
+    /**
+     * @var string
+     */
+    private $iconTag='span';
+
+    /**
+     * Parses the given string and replaces all occurrences of .icon-[name] with the corresponding icon.
+     *
+     * @param string $text The text to parse
+     *
+     * @return string The HTML code with the icons
+     */
+    public function parseIconsFilter($text)
+    {
+        $that = $this;
+
+        return preg_replace_callback(
+            '/\.([a-z]+)-([a-z0-9+-]+)/',
+            function ($matches) use ($that) {
+                return $that->iconFunction($matches[2], $matches[1]);
+            },
+            $text
+        );
+    }
+
+    /**
+     * Returns the HTML code for the given icon.
+     *
+     * @param string $icon The name of the icon
+     * @param string $iconSet The icon-set name
+     *
+     * @return string The HTML code for the icon
+     */
+    public function iconFunction($icon, $iconSet = 'icon')
+    {
+        if ($iconSet == 'icon') $iconSet = $this->iconPrefix;
+        $icon = str_replace('+', ' '.$iconSet.'-', $icon);
+
+        return sprintf('<%1$s class="%2$s %2$s-%3$s"></%1$s>', $this->iconTag, $iconSet, $icon);
+    }
 
 }
