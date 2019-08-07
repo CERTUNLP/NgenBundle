2bed001cb7d259285f6670d673b1187dfc8d0f05
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2019-08-07 17:31:54 -0300

    trabajando con los dashboard

---
+++ DoctrineMigrations/Version20190807183208.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20190807183208 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE incident ADD response_dead_line DATETIME NULL, ADD solve_dead_line DATETIME NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE incident DROP FOREIGN KEY FK_3D03A11AEC6344B7');
+        $this->addSql('ALTER TABLE incident DROP response_dead_line, DROP solve_dead_line');
+        $this->addSql('ALTER TABLE incident_decision DROP FOREIGN KEY FK_7C69DA3B3AA33DF6');
+        $this->addSql('ALTER TABLE incident_decision RENAME INDEX idx_7c69da3b3aa33df6 TO IDX_7C69DA3B7DC9D7A5');
+    }
+}
