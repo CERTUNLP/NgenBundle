4e57b6172e0b80cafc27c6646cf93553bf56efdf
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2019-08-07 13:20:19 -0300

    Closes:#79

--- DoctrineMigrations/Version20190627174812.php
+++ DoctrineMigrations/Version20190627174812.php
@@ -19,8 +19,8 @@ class Version20190627174812 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE incident_priority ADD unresponse_time INT NOT NULL, ADD unresolution_time INT NOT NULL');
-        $this->addSql("INSERT INTO incident_state (`slug`, `name`, `is_active`, `created_at`, `updated_at`, `mail_admin`, `mail_reporter`, `mail_assigned`, `mail_team`, `incident_state_action`) VALUES ('closed-by-unresolved', 'Closed by unresolved', '1', NOW(), NOW(), 'none', 'none', 'none', 'none', 'close')");
-        $this->addSql("INSERT INTO incident_state (`slug`, `name`, `is_active`, `created_at`, `updated_at`, `mail_admin`, `mail_reporter`, `mail_assigned`, `mail_team`, `incident_state_action`) VALUES ('closed-by-unattended', 'Closed by unattended', '1', NOW(), NOW(), 'none', 'none', 'none', 'none', 'close')");
+        $this->addSql("INSERT INTO incident_state (`slug`, `name`, `is_active`, `created_at`, `updated_at`, `mail_admin`, `mail_reporter`, `mail_assigned`, `mail_team`, `incident_state_action`) VALUES ('closed_by_unresolved', 'Closed by unresolved', '1', NOW(), NOW(), 'none', 'none', 'none', 'none', 'close')");
+        $this->addSql("INSERT INTO incident_state (`slug`, `name`, `is_active`, `created_at`, `updated_at`, `mail_admin`, `mail_reporter`, `mail_assigned`, `mail_team`, `incident_state_action`) VALUES ('closed_by_unattended', 'Closed by unattended', '1', NOW(), NOW(), 'none', 'none', 'none', 'none', 'close')");
 
     }
 
