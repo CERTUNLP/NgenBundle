61ab1955061142013179df8152c935c3f988abb4
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2019-08-08 18:59:46 -0300

    acomodado la hora

---
+++ DoctrineMigrations/Version20190808204749.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20190808204749 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('update incident set response_dead_line=DATE_ADD(created_at, INTERVAL 30 DAY), solve_dead_line=DATE_ADD(created_at, INTERVAL 60 DAY) where solve_dead_line is null and response_dead_line is null;');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+    }
+}
