874a5436c36ace20d5a46eee058529d251a1c29d
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2019-08-14 17:17:57 -0300

    modificadas multiples vistas y ordenamientos, es necesario pupoluar los indices

--- Resources/public/js/incident/Incident.js
+++ Resources/public/js/incident/Incident.js
@@ -7,6 +7,13 @@
  * with this source code in the file LICENSE.
  */
 var Incident = Frontend.extend({
+    init: function () {
+        this.eventTarget = null;
+    $('.select-filter').on('change', $.proxy(this.search, this));
+    $('.multiple-select-filter').on('blur', $.proxy(this.search, this));
+    $('.data-filter').on('submit', $.proxy(this.search, this));
+    $('.generalSearch').on('submit', $.proxy(this.search, this));
+},
     dropDownChangeLinks: function () {
         if (this.eventTarget.data('state-slug') == "open") {
             this.eventTarget.hide();
