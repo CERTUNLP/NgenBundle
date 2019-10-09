b06f73e047af2090d0a90f6ce6bbb8eba34eb4ae
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2019-10-09 11:57:45 -0300

    cmabios en Type

--- DoctrineMigrations/Version20190917154226.php
+++ DoctrineMigrations/Version20190917154226.php
@@ -17,9 +17,10 @@ class Version20190917154226 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('UPDATE state_behavior SET slug="closed" WHERE slug="close"');
-        $this->addSql('UPDATE state_behavior SET slug="discarded" WHERE slug="discard"');
-        $this->addSql('UPDATE state_behavior SET slug="on_treatment", name="On Treatment" WHERE slug="open"');
+        $this->addSql('UPDATE state_behavior SET slug="closed", is_active=1, discr="closed" WHERE slug="close"');
+        $this->addSql('UPDATE state_behavior SET slug="discarded",is_active=1, discr="discarded" WHERE slug="discard"');
+        $this->addSql('UPDATE state_behavior SET slug="on_treatment", name="On Treatment", is_active=1,can_edit=1,can_enrich=1,can_add_history=1,discr="on_treatment" WHERE slug="open"');
+        $this->addSql('UPDATE state_behavior SET  is_active=1,can_edit=1,can_enrich=1,can_add_history=1,can_edit_fundamentals=1,discr="new" WHERE slug="new"');
         $this->addSql('DELETE FROM incident_change_state where state_edge_id is null');
         $this->addSql("UPDATE incident_tlp SET name='amber' WHERE slug='amber'");
         $this->addSql("UPDATE incident_tlp SET name='green' WHERE slug='green';");
