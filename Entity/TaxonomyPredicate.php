39f1d10fbed23aeed4d91b7f2b995527e92cbcf0
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2019-10-09 14:52:27 -0300

    yendo

---
+++ Entity/TaxonomyPredicate.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace CertUnlp\NgenBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * tax_predicate
+ *
+ * @ORM\Table(name="taxonomy_predicate")
+ */
+class TaxonomyPredicate
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="expanded", type="string", length=255)
+     */
+    private $expanded;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="version", type="integer")
+     */
+    private $version;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="string", length=255, unique=true)
+     */
+    private $value;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return taxonomyPredicate
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set expanded
+     *
+     * @param string $expanded
+     *
+     * @return taxonomyPredicate
+     */
+    public function setExpanded($expanded)
+    {
+        $this->expanded = $expanded;
+
+        return $this;
+    }
+
+    /**
+     * Get expanded
+     *
+     * @return string
+     */
+    public function getExpanded()
+    {
+        return $this->expanded;
+    }
+
+    /**
+     * Set version
+     *
+     * @param integer $version
+     *
+     * @return taxonomyPredicate
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+
+        return $this;
+    }
+
+    /**
+     * Get version
+     *
+     * @return int
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     *
+     * @return taxonomyPredicate
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return taxonomyPredicate
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
+
