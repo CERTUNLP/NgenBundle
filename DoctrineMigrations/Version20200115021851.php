ff7f9e500c7b9d9e09219e047f06f3286b001ec6
Einar Lanfranco <einar@mail.info.unlp.edu.ar>
2020-01-14 23:49:38 -0300

    listo el manejo de los estados

--- DoctrineMigrations/Version20200115021851.php
+++ DoctrineMigrations/Version20200115021851.php
@@ -19,11 +19,8 @@ class Version20200115021851 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE communication_behavior CHANGE mode mode ENUM(\'manual\',\'file\',\'data\', \'all\')');
-        $this->addSql('ALTER TABLE incident_decision CHANGE when_to_update when_to_update VARCHAR(100) NOT NULL');
-        $this->addSql('ALTER TABLE incident_comment_thread ADD CONSTRAINT FK_E073862F59E53FB9 FOREIGN KEY (incident_id) REFERENCES incident (id)');
-        $this->addSql('ALTER TABLE incident_change_state ADD CONSTRAINT FK_CCFC5A1D59E53FB9 FOREIGN KEY (incident_id) REFERENCES incident (id)');
-        $this->addSql('ALTER TABLE incident_detected CHANGE when_to_update when_to_update VARCHAR(100) NOT NULL');
-        $this->addSql('ALTER TABLE incident_detected ADD CONSTRAINT FK_F997633159E53FB9 FOREIGN KEY (incident_id) REFERENCES incident (id)');
+        $this->addSql('ALTER TABLE incident_decision CHANGE when_to_update when_to_update VARCHAR(100) NULL default "live"');
+        $this->addSql('ALTER TABLE incident_detected CHANGE when_to_update when_to_update VARCHAR(100) NULL default "live"');
     }
 
     /**
