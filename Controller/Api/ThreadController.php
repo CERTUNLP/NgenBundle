9b4f3924bafea90219d6f0a850b5c2191e7665e5
dam <drubio@cert.unlp.edu.ar>
2020-08-24 20:49:25 -0300

    incident comment apikey

--- Controller/Api/ThreadController.php
+++ Controller/Api/ThreadController.php
@@ -12,7 +12,11 @@
 namespace CertUnlp\NgenBundle\Controller\Api;
 
 use FOS\CommentBundle\Controller\ThreadController as BaseThreadController;
+use FOS\CommentBundle\Model\CommentInterface;
+use FOS\RestBundle\View\View;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Restful controller for the Threads.
@@ -30,5 +34,19 @@ class ThreadController extends BaseThreadController
 
     public function setContainer(\Psr\Container\ContainerInterface $container)
     {
+    }
+
+    /**
+     * Forwards the action to the comment view on a successful form submission.
+     *
+     * @param FormInterface $form Form with the error
+     * @param string $id Id of the thread
+     * @param CommentInterface $parent Optional comment parent
+     *
+     * @return View
+     */
+    protected function onCreateCommentSuccess(FormInterface $form, $id, CommentInterface $parent = null)
+    {
+        return View::createRouteRedirect('fos_comment_get_thread_comment', ['id' => $id, 'commentId' => $form->getData()->getId(), 'apikey' => $this->getUser()->getApikey()], Response::HTTP_CREATED);
     }
 }
