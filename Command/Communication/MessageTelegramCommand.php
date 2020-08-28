0db2d39565ed8447fb630fa6fa57a298496132c9
dam <drubio@cert.unlp.edu.ar>
2020-08-28 15:01:01 -0300

    message command added recipents for responses

--- Command/Communication/MessageTelegramCommand.php
+++ Command/Communication/MessageTelegramCommand.php
@@ -2,14 +2,12 @@
 
 /*
  * This file is part of the Ngen - CSIRT Incident Report System.
- *
- * (c) CERT UNLP <support@cert.unlp.edu.ar>
- *
- * This source file is subject to the GPL v3.0 license that is bundled
- * with this source code in the file LICENSE.
+ *   (c) CERT UNLP <support@cert.unlp.edu.ar>
+ *  This source file is subject to the GPL v3.0 license that is bundled
+ *  with this source code in the file LICENSE.
  */
 
-namespace CertUnlp\NgenBundle\Command;
+namespace CertUnlp\NgenBundle\Command\Communication;
 
 use CertUnlp\NgenBundle\Repository\Communication\Message\MessageTelegramRepository;
 use Doctrine\ORM\EntityManagerInterface;
