6129e1e8dfbe61b308f62be1b485b434ce69fc77
dam <drubio@cert.unlp.edu.ar>
2021-02-01 13:20:23 -0300

    validation of deadline states on incident decision form frontend  #153

--- Resources/public/js/incident/decision/IncidentDecisionForm.js
+++ Resources/public/js/incident/decision/IncidentDecisionForm.js
@@ -9,6 +9,7 @@
 var IncidentDecisionForm = Form.extend({
     config: function () {
         this.setIncidentDecisionId();
+        $("#state").on("change", $.proxy(this.getNextStates, this));
     },
     getObjectBrief: function () {
         return 'incident/decision';
@@ -21,6 +22,30 @@ var IncidentDecisionForm = Form.extend({
     },
     getIncidentDecisionId: function () {
         return this.incident_decision_id;
-    }
+    },
+    getNextStates: function (event, args) {
+        var $id = $("#state option:selected").val();
+        this.laddaButton = Ladda.create(this.getSubmitButton().get(0));
+        this.laddaButton.start();
+        $.publish('/cert_unlp/incident/state/read', [$id, $.proxy(this.changeDeadlineStates, this)]);
+        this.laddaButton.stop();
+
+    },
+    changeDeadlineStates: function (response) {
+        if (response.responseJSON && response.responseJSON.length) {
+            if (response.responseJSON[0].hasOwnProperty('new_states_slug')) {
+                $("#unrespondedState").empty()
+                $("#unsolvedState").empty()
+                response.responseJSON[0].new_states_slug.forEach(function ($state) {
+                    $("#unrespondedState").append('<option value=' + $state['slug'] + '>' + $state['name'] + '</option>');
+                    $("#unsolvedState").append('<option value=' + $state['slug'] + '>' + $state['name'] + '</option>');
+                })
+
+            }
+
+            $("#unrespondedState").trigger('change.select2')
+            $("#unsolvedState").trigger('change.select2')
+        }
+    },
 });
 
